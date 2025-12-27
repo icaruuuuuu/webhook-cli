5d9925438ed92a1495a0bc842bae160b39357a42
icaruuuuuu <icaromachadosilva@gmail.com>
2025-12-27 15:15:03 -0300

    feat: basic app functionality

---
+++ webhook-cli.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import argparse
+import os
+
+def readMd(file):
+    with open(file) as f:
+        ...
+
+def makeJson(args, usefile):
+    if usefile:
+        msg = readMd(args.file)
+    else:
+        msg = args.message
+
+    with open('tmp.json', 'w') as t:
+        t.write('{\n\t"content":' + f'"{msg}"' + '\n}\n')
+        
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--message", help="Message to be sent to the webhook.")
+    parser.add_argument("-f", "--file", help="Markdown file to be sent to the webhook. Alternative to -m if your message is too complex")
+    parser.add_argument("url", help="Discord webhook URL")
+
+    args = parser.parse_args()
+    usefile = True if args.file else False
+    url = args.url
+
+    if usefile:
+        print("Feature not supported yet")
+        return
+
+    makeJson(args, usefile)
+
+    exit_status = os.system(f'curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data @tmp.json {url}')
+    print(exit_status)
+
+
+if __name__ == "__main__":
+    main()
